81d106ca22986747092a7a77211992a5f1e18742
Hector Orantes <orantesroman39@gmail.com>
2024-07-27 11:15:36 -0600

    [Add] Delete Register

--- src/components/organisms/SectionLogin.jsx
+++ src/components/organisms/SectionLogin.jsx
@@ -69,9 +69,6 @@ function SecionLogin() {
             <div className="w-1/2 block px-3 py-2 m-3 bg-[#e1ecf4] rounded-[3px] border border-[#7aa7c7] shadow-[inset_0_1px_0_0_rgba(255,255,255,0.7)] box-border text-[#39739d] cursor-pointer inline-block text-[13px] font-normal leading-[1.15385] m-0 outline-none py-2 px-[0.8em] relative text-center no-underline select-none align-baseline whitespace-nowrap hover:bg-[#b3d3ea] hover:text-[#2c5777] focus:bg-[#b3d3ea] focus:text-[#2c5777] focus:shadow-[0_0_0_4px_rgba(0,149,255,0.15)] active:bg-[#a0c7e4] active:shadow-none active:text-[#2c5777] border rounded-lg shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 sm:text-sm focus:ring-1">
                 <Button title="Iniciar Sesión" onClick={handleClick}></Button>
             </div>
-            <div>
-                
-            </div>
         </div>
         </form>
     );
